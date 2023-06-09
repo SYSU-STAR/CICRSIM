50da567e887d89192dc1a6b363a69da870237cae
Zixuan Zhuang <1094375872@qq.com>
2023-06-10 00:28:31 +0800

    update

--- sim_env.sh
+++ sim_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo cp -r ./apriltag ~/.gazebo/models
-sudo unzip ./models.zip -d ~/.gazebo/models
+sudo unzip ./models.zip -d ~/.gazebo
+sudo cp -r ./apriltag ~/.gazebo/models/apriltag
 sudo cp -r ./files ~/YOUR_WORKSPACE_PATH
 sudo cp -r ./start_simulation.sh ~/YOUR_WORKSPACE_PATH
 
